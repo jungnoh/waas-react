b8f71f1a57e42aa0f80d0971e8073c3393441455
김태훈[ 학부재학 / 사이버국방학과 ] <taehoon1310@korea.edu>
2019-12-31 15:45:28 +0900

    day 1 Done

--- day1/base-name/src/components/SayHi.jsx
+++ day1/base-name/src/components/SayHi.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'; //React를 쓰기 위해 import
 
-const SayHi = (props) => {
+const SayHi = (props) => { //함수를 통해 component 생성
   return (
-    <div>님 안녕하세요.</div>
+    <div>{props.name}님 안녕하세요.</div>
   );
 }
 
-export default SayHi;
+export default SayHi; //다른 파일에서 쓸 수 있도록 SayHi라는 이름으로 export
--- day1/base-name/src/index.js
+++ day1/base-name/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
+//react를 쓰기 위해 필수적인 작업
 import ReactDOM from 'react-dom';
+//실제로 그려야 하기 때문에 import
 import Main from './components/Main';
+//Main을 render하기 위해 import한다.
 
 const mainEl = document.getElementById('main');
+// index.html에서 id가 main인 div를 찾는다.
+
 ReactDOM.render(<Main />, mainEl);
+// 찾은 곳에 Main.jsx에서 import한 Main을 그려준다.
