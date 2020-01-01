b68a53c3c78397d63d8d65d69aefecd8e33eebee
namkim il <namil000208@gmail.com>
2020-01-01 15:22:37 +0900

    day2

--- day1/base-name/src/components/NameInput.jsx
+++ day1/base-name/src/components/NameInput.jsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 
 const NameInput = (props) => {
+  const [name, setname] = useState('');
   const val = '';
   return (
     <div>
-      <input val={val} />
-      <button>Change</button>
+      <input val={name} onChange={event=>{console.log(event.target.value),setname(event.target.value)}}/>
+      <button onClick={event=>{alert(name+'님 안녕하세요.')}}>Change</button>
     </div>
   )
 }
