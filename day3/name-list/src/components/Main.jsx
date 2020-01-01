e64da7f5ef2d3c602843003e4da4bbe8e5339ae1
ChungHwann <cndghks15@korea.ac.kr>
2020-01-01 20:46:51 +0900

    day3

--- day3/name-list/src/components/Main.jsx
+++ day3/name-list/src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import NameDisplay from './NameDisplay';
+//import NameDisplay from './NameDisplay';
 import NameInput from './NameInput';
+import NameDisplay from './EmptyNameDisplay'
 
 const Main = () => {
   const [nameList, setNameList] = useState([]);
@@ -11,16 +12,44 @@ const Main = () => {
   };
   // NameInput에서 input을 클릭했을 때 처리할 함수
   const onAddClick = (name) => {
+    addName(name);
     alert(name);
   };
-
+  //props.children
+  const child = (name) => {
+    return (
+      <div className="name-item">
+        - {name}
+      </div>
+    );
+  };
+  const renderNameList = [];
+  for (let i=0; i<nameList.length; i++){
+    renderNameList.push(
+      <NameDisplay children={child(nameList[i])} key={i}/>
+    )
+  }
+  /*
+  //for 
+  const renderNameList = [];
+  for (let i=0; i<nameList.length; i++){
+    renderNameList.push(
+      <NameDisplay name={nameList[i]} key={i}/>
+    )
+  }
+  */
+  /*
+  //map
+  const renderNameList = nameList.map((name, index) =>
+    <NameDisplay name={name} key={index}/>
+  );
+  */
   return (
     <div className="container">
-      <span className="title">Name List</span>
+      <span className="title blue-text">Name List</span>
       <NameInput addClick={onAddClick} />
       <div className="list-container">
-        <NameDisplay name="명순구" />
-        <NameDisplay name="정진택" />
+        {renderNameList}
       </div>
     </div>
   );
