4ca712d171c5e860d29319656ee780d3f298fba2
Youngwoo Kimh <dankimh@naver.com>
2020-01-03 20:32:45 +0900

    2nd week

--- day1/base-name/src/components/NameInput.jsx
+++ day1/base-name/src/components/NameInput.jsx
@@ -1,42 +1,50 @@
 import React, {useState} from 'react';
 
-/*const NameInput = (props) => {
-  const val = 'aa';
-  props={val:{val}};
-  function onChanged() {
-    console.log('this is : ', props.val);
+//using function statement
+const NameInput = (props) => {
+  const val='dd';
+  const [name,changeName]=useState('');
+  function onChanged(e) {
+    alert(name+'님 안녕하세요');
+
+  }
+  const onChangeName = (e) => {
+    changeName(e.target.value);
   }
   return (
     <div>
-      <input val={val} />
+      <input value={name} onChange={onChangeName}/>
       <button onClick={onChanged}>Change</button>
     </div>
   )
-}*/
-class NameInput extends React.Component{
+}
+
+//using class statement
+/*class NameInput extends React.Component{
   constructor(props){
     super(props);
+    this.state={val: ''};
     this.onChanged=this.onChanged.bind(this);
     this.what=this.what.bind(this);
-    this.state={val: ' ' };
+    
+  }
+  what(e){
+    this.setState({val:e.target.value});
   }
   onChanged(e){
     //this.setState({val:e.target.val});
-    console.log(this.state.val);
+    console.log(this.state);
     e.preventDefault();
   }
-  what(e){
-    this.setState({val:e.target.val});
-  }
+  
   render(){
-    //const val='ss';
     return(
       <div>
-        <input type="text" value={this.state.val||''} onChange={this.what} />
+        <input type="text" value={this.state.val} onChange={this.what} />
         <button onClick={this.onChanged}>Change</button>
      </div>
-    )
+    );
   }
-}
+}*/
 
 export default NameInput;
