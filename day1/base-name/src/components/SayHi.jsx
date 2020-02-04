0326831b63dff34379ea87b16fb3495da40814db
regitiger98 <regitiger98@naver.com>
2020-02-04 22:10:02 +0900

    HW 1

--- day1/base-name/src/components/SayHi.jsx
+++ day1/base-name/src/components/SayHi.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'; // react 모듈 import
 
-const SayHi = (props) => {
+const SayHi = (props) => { // SayHi 컴포넌트를 선언함
   return (
-    <div>님 안녕하세요.</div>
+  <div>{props.name}님 안녕하세요.</div>
   );
 }
 
-export default SayHi;
+export default SayHi; // SayHi 컴포넌트를 다른 곳에서 사용할 수 있도록 함
